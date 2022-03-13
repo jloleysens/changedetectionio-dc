b388d67e2f08a7d7b487fcff3a46d98acea6b989
Jean-Louis Leysens <jloleysens@gmail.com>
2022-03-13 23:00:24 +0100

    updated generate ssl cert script

--- generate-ssl-cert.sh
+++ generate-ssl-cert.sh
@@ -1,10 +1,13 @@
 # Roughly based on this guide https://www.digitalocean.com/community/tutorials/how-to-create-a-self-signed-ssl-certificate-for-nginx-on-centos-7
 
+# 0. Create ignored certs folder
 mkdir certs
 
 # 1. Create key and cert files
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
-# 2. Make the key file 700
-sudo chmod 700 certs/server.key
+# 2. Make the key file readable by nginx
+sudo chmod 664 certs/server.key
 # 3. For "Perfect Forwarding Secrecy" negotiation with clients
 sudo openssl dhparam -out certs/dhparam.pem 2048
+
+# then scp files up to remote
